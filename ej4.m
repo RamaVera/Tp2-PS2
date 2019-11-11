aa7b7f4e9a58976dd11260ea599d66f602c8de60
Gabriel Machado <gadu.machado@gmail.com>
2019-11-10 23:24:30 -0300

    Create ej4.m

---
+++ ej4.m
@@ -0,0 +1,66 @@
+clear all; close all; clc
+
+load imagenes.mat
+load patrones.mat
+
+gamma=1; lambda=8; sigma=4; theta=0; psi=0;
+gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'e');
+
+
+figure
+l_vec=[4 7 10 13];
+for i=1:4
+    lambda=l_vec(i);
+    gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'e');
+    f_lenna=filter2(gb,foto_lenna);
+    
+    %par
+    subplot(2,4,i)
+    imshow(mat2gray(f_lenna))
+    title(['par \lambda=', num2str(lambda)])
+    %impar
+    gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'o');
+    f_lenna=filter2(gb,foto_lenna);
+    subplot(2,4,i+4)
+    imshow(mat2gray(f_lenna))
+    title(['impar \lambda=', num2str(lambda)])
+end
+
+lambda=8; s_vec=[2 4 6 8];
+figure
+for i=1:4
+    sigma=s_vec(i);
+    gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'e');
+    f_lenna=filter2(gb,foto_lenna);
+    
+    %par
+    subplot(2,4,i)
+    imshow(mat2gray(f_lenna))
+    title(['par \sigma=', num2str(sigma)])
+    %impar
+    gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'o');
+    f_lenna=filter2(gb,foto_lenna);
+    subplot(2,4,i+4)
+    imshow(mat2gray(f_lenna))
+    title(['impar \sigma=', num2str(sigma)])
+end
+
+sigma=4; t_vec=[0 pi/4 pi/2 pi*3/4]; 
+pi_vec={'0' '\pi/4' '\pi/2' ' 3\pi/4'}
+figure
+for i=1:4
+    theta=t_vec(i);
+    gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'e');
+    f_lenna=filter2(gb,foto_lenna);
+    
+    %par
+    subplot(2,4,i)
+    imshow(mat2gray(f_lenna))
+    title(['par \theta=', pi_vec{i}])
+    %impar
+    gb=gabor_fn(sigma,theta,lambda,psi,gamma, 'o');
+    f_lenna=filter2(gb,foto_lenna);
+    subplot(2,4,i+4)
+    imshow(mat2gray(f_lenna))
+    title(['impar \theta=', pi_vec{i}])
+end
